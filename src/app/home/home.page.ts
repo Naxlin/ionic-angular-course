c95b28b058653913f4a3d3bdbfa1a59b5e89f3e6
naxlin <naxlin.everthaine@gmail.com>
2021-01-23 11:58:08 -0700

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  text = "Default starting text";
 
   constructor() {}
+
+  onChangeText() {
+    this.text = 'Changed!';
+  }
 
 }
