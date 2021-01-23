89d20a127ab033e272b2cfdbecdd720889633f1c
naxlin <naxlin.everthaine@gmail.com>
2021-01-23 13:38:53 -0700

    lots of progress

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,24 +8,12 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [
-    {
-      id: 'r1',
-      title: 'Schnitzel',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Wiener_Schnitzel_in_Wachwitz_%281%29.JPG/1280px-Wiener_Schnitzel_in_Wachwitz_%281%29.JPG',
-      ingredients: ['French Fries', 'Pork Meat', 'Salad']
-    },
-    {
-      id: 'r2',
-      title: 'Spaghetti',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Spaghetti-prepared.jpg/1280px-Spaghetti-prepared.jpg',
-      ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
-    }
-  ]
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+    this.recipes = this.recipesService.getRecipes();
   }
 
 }
---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Wiener_Schnitzel_in_Wachwitz_%281%29.JPG/1280px-Wiener_Schnitzel_in_Wachwitz_%281%29.JPG',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Spaghetti-prepared.jpg/1280px-Spaghetti-prepared.jpg',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
+    }
+  ];
+
+  constructor() { }
+
+  getRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(id: string) {
+    return {...this.recipes.find(recipe => {
+      return recipe.id === id;
+    })
+  }}
+
+  deleteRecipe(id: string) {
+    this.recipes = this.recipes.filter(recipe => {
+      return recipe.id !== id;
+    });
+  }
+}
